8ab27062554948a5305e366a68f605625edf7553
teekay <tkikuchi2000@gmail.com>
2018-07-08 23:42:20 +0900

    rebuilding site Sun, Jul  8, 2018 11:42:20 PM

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -x
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+# hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hugo-fresh
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add -A 
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+cd ..
+git subtree push --prefix=public https://github.com/tkikuchi2000/hugo-fresh.git gh-pages
